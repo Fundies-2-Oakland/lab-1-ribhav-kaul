ef7cd5178c79186c1683bb8abebcebda9820bd22
Ribhav Kaul <ribhavkaul@gmail.com>
2025-01-09 17:09:43 -0800

    Worked on 4 problems since it implied to do 2 5ths of the 10 problems.

--- Main.java
+++ Main.java
@@ -1,6 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("This is my first Java program!");
+        Main m = new Main(1.0, 2.0, 3.0);
+        System.out.println(m.getX() + " " + m.getY() + " " + m.getZ());
+        System.out.println(m.toString());
+        System.out.println(m.getMagnitude());
+    }
+    double x;
+    double y;
+    double z;
+    public Main (double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getZ() {
+        return z;
+    }
+    public String toString() {
+        String roundedX = String.format("%.2f", x);
+        String roundedY = String.format("%.2f", y);
+        String roundedZ = String.format("%.2f", z);
+        return "(" + roundedX + ", " + roundedY + ", " + roundedZ + ")";
+    }
+    public double getMagnitude() {
+        return Math.sqrt(x*x + y*y + z*z);
     }
 }
